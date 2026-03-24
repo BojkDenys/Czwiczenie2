f65e5a70dedf57be9b0b83798571d4e107658473
Denys Boiko <denisbojko2005@gmail.com>
2026-03-24 14:01:33 +0100

    add class Printer and create there 2 methods for electronic

---
+++ Czwiczenie2/Util/Printer.cs
@@ -0,0 +1,46 @@
+﻿using Czwiczenie2.Models;
+using Czwiczenie2.Service;
+
+namespace Czwiczenie2.Util;
+
+public class Printer
+{
+    private readonly ElectronicService _electronicService;
+    private readonly RentService _rentService;
+
+    public Printer(ElectronicService electronicService, RentService rentService)
+    {
+        _electronicService = electronicService;
+        _rentService = rentService;
+    }
+
+    public void showAllElectronic()
+    {
+        Console.WriteLine("All electronic: ");
+        List<Electronic> electronics = _electronicService.GetAll();
+        if (electronics.Count == 0)
+        {
+            Console.WriteLine("Empty");
+        }
+
+        foreach (Electronic electronic in electronics)
+        {
+            Console.WriteLine(electronic);
+        }
+    }
+
+    public void PrintFreeElectronic()
+    {
+        Console.WriteLine("Free electronic:");
+        List<Electronic> electronics = _electronicService.GetAllFree();
+        if (electronics.Count == 0)
+        {
+            Console.WriteLine("Empty");
+        }
+
+        foreach (Electronic electronic in electronics)
+        {
+            Console.WriteLine(electronic);
+        }
+    }
+}
